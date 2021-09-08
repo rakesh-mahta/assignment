3cf6e7486c48065700e57a0fe70817d76fb91907
rakesh-mahta <rakesh.mahta@timeinc.com>
2021-09-08 17:24:45 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ from prettytable import PrettyTable
 ec2 = boto3.resource('ec2',region_name='us-east-1')
 
 def lambda_handler(event, context):  
-        running_instances = ec2.instances.filter(
+    running_instances = ec2.instances.filter(
     Filters=[{
         'Name': 'instance-type',
         'Values': ['m5.large']
@@ -19,26 +19,26 @@ def lambda_handler(event, context):
         }]
     )
 
-L=[]
-M=[]
-ec2info = defaultdict()
-for instance in running_instances:
-    for tag in instance.tags:
-        if 'Name'in tag['Key']:
-            name = tag['Value']
+    L=[]
+    M=[]
+    ec2info = defaultdict()
+    for instance in running_instances:
+        for tag in instance.tags:
+            if 'Name'in tag['Key']:
+                name = tag['Value']
     # Add instance info to a dictionary         
-    ec2info[instance.id] = {
-        'Name': name,
-        'Type': instance.instance_id
-        }
-    L.append(name)
-    M.append(instance.instance_id)
+        ec2info[instance.id] = {
+            'Name': name,
+            'Type': instance.instance_id
+            }
+        L.append(name)
+        M.append(instance.instance_id)
 
 # Format output in table from
 
-table = PrettyTable(['Name Tag', 'Instance ID'])
-for i in range(len(L)):
-  table.add_row([L[i], M[i]])
-print(table)
+    table = PrettyTable(['Name Tag', 'Instance ID'])
+    for i in range(len(L)):
+        table.add_row([L[i], M[i]])
+    print(table)
 
 
